465e1cdc4b8f595fe108b014c2a6aabe5e49c591
Ziyang Ruan <ruanziyang@gmail.com>
2018-01-28 22:31:25 -0800

    finished lesson1 sunshine

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,41 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.android.sunshine.data.SunshinePreferences;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a field to store the weather display TextView
+    TextView weatherview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
+        weatherview = (TextView) findViewById(R.id.tv_weather_data);
         // TODO (2) Use findViewById to get a reference to the weather display TextView
-
+        String[] weatherdata = {
+                "Today, Jan 28 - Clear - 13°C / 9°C",
+                "Tomorrow - Cloudy - 17°C / 7°C",
+                "Tuesday - Sunny - 17°C / 6°C",
+                "Wednesday - Partly Cloudy - 17°C / 7°C",
+                "Thursday - Partly Cloudy - 18°C / 8°C",
+                "Friday - Sunny - 19°C / 9°C",
+                "Saturday - Sunny - 15°C / 10°C",
+                "Sunday - Sunny - 16°C / 7°C",
+                "Monday - Cloudy - 19°C / 15°C",
+                "Tuesday - Rainy - 12°C / 11°C",
+                "Wednesday - Partly Rainy - 15°C / 9°C",
+                "Thursday - Sunny - 16°C / 7°C",
+                "Friday - Rainy - 16°C / 8°C",
+                };
         // TODO (3) Create an array of Strings that contain fake weather data
-
+        for(String eachdata: weatherdata){
+            weatherview.append(eachdata + "\n\n\n\n");
+        }
         // TODO (4) Append each String from the fake weather data array to the TextView
     }
 }
